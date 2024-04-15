83a635830a57baa77fd4a49b0316a24696f7ea3b
Joseph Mi <jmjosephmi@gmail.com>
2024-04-15 18:18:55 -0400

    added emails and photos

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Welcome to Reach Out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:jm.josephmi@email.com">jm.josephmi@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/joseph-mi/">www.linkedin.com/in/joseph-mi</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/Joseph-Mi">github.com/Joseph-Mi</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
